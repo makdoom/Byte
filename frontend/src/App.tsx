4c12e08906417a34d57d50a138a157bb7f177da4
Makdoom Shaikh <makshaikh99@gmail.com>
2024-07-20 20:23:29 +0530

    feat: Add vite react frontend with tailwindcss & shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600">Medium</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
