9154746912cdfd53190f45fa0cafcbfecf876c55
Makdoom Shaikh <makshaikh99@gmail.com>
2024-07-27 23:30:47 +0530

    feat: Add blocknot editor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Route, Routes } from "react-router";
 import { Home } from "@/pages/Home";
 import { DraftBlog } from "@/pages/DraftBlog";
--- frontend/src/components/BlockEditor.tsx
+++ frontend/src/components/BlockEditor.tsx
@@ -1,7 +1,40 @@
+import "@blocknote/core/fonts/inter.css";
+import "@blocknote/mantine/style.css";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@/styles/custom-editor.css";
+
 import { Button } from "@/components/ui/button";
 import { Captions, Image } from "lucide-react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+// import { Block } from "@blocknote/core";
 
 export const BlockEditor = () => {
+  const [blogTitle, setBlogTitle] = useState("");
+  // const [blocks, setBlocks] = useState<Block[]>([]);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const editor = useCreateBlockNote({
+    initialContent: undefined,
+    uploadFile: async (file: File) => {
+      console.log("hello", file);
+      return "hello";
+    },
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    if (e.target.value.length <= 100) {
+      setBlogTitle(e.target.value);
+    }
+  };
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [blogTitle]);
+
   return (
     <div className="">
       <div className=" h-[calc(100vh-90px)] overflow-scroll max-w-screen-xl m-auto no-scrollbar mt-5">
@@ -18,7 +51,26 @@ export const BlockEditor = () => {
             </Button>
           </div>
 
-          
+          <textarea
+            ref={textareaRef}
+            value={blogTitle}
+            onChange={handleChange}
+            placeholder="Blog Title..."
+            rows={1}
+            className="w-full resize-none overflow-hidden mt-4 font-bold text-4xl outline-none placeholder:text-muted-foreground"
+          />
+
+          <div className="relative !-left-12 mt-4">
+            <BlockNoteView
+              editor={editor}
+              theme={"light"}
+              // onChange={() => {
+              //   setBlocks(editor.document);
+              // }}
+              className="text-primary"
+            />
+          </div>
+          {/* {JSON.stringify(blocks, null, 2)} */}
         </div>
       </div>
     </div>
